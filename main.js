7d2c8e6279e44ebee2c6236b6182cd3d4feb6dbc
FilipBednarek <131236640+FilipBednarek@users.noreply.github.com>
2023-12-07 11:25:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,46 +13,45 @@ btnNext.addEventListener('click', function (e) {
   const results = document.querySelector('#results');
   // const percentage ;
 
-  const isCoalitionvalid() {
-
-  }
-
-
   const object = {
     electoralname: electoralname.value,
-    isCoalition: isCoalition.value,
+    isCoalition: isCoalition.cheked ? isCoalition.value : 8,
     numberOfVotes: numberOfVotes.value,
   };
 
-  electoralList.innerHTML += `<li><b>${electoralname.value}</b>, ${isCoalition.value}, ilość głosów:${numberOfVotes.value}</li>`;
+  electoralList.innerHTML += `<li><b>${electoralname.value}</b>, ${
+    object.isCoalition ? 'jest koalicją ' : 'nie jest koalicją'
+  }, ilość głosów:${numberOfVotes.value}</li>`;
 
   array.push(object);
-
-  console.log(isCoalition.value);
-  console.log('-----------------');
-  console.log(object);
 });
 
 btnCalc.addEventListener('click', function (e) {
   e.preventDefault();
 
+  const electoralname = document.querySelector('#electoralName');
+  const isCoalition = document.querySelector('#isCoalition');
+  const numberOfVotes = document.querySelector('#numberOfVotes');
+  const electoralList = document.querySelector('#electoralList');
+  const results = document.querySelector('#results');
+  // const percentage ;
+
   results.innerHTML += `
   
     <table>
-            <tr>
-              <th>Miejsce</th>
-              <th>Nazwa ugrupowania</th>
-              <th>Próg wyborczy</th>
-              <th>Ilość głosów</th>
-              <th>Wynik procentowy</th>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>${electoralname.value}</td>
-              <td>${isCoalition.value}</td>
-              <td>${numberOfVotes.value}</td>
-              <td>34,7%</td>
-            </tr>
-  
-          </table>`;
+    <tr>
+    <th>Miejsce</th>
+    <th>Nazwa ugrupowania</th>
+    <th>Próg wyborczy</th>
+    <th>Ilość głosów</th>
+    <th>Wynik procentowy</th>
+  </tr>
+        <tr>
+            <td>1</td>
+            <td>${electoralname.value}</td>
+            <td>${isCoalition.value}</td>
+            <td>${numberOfVotes.value}</td>
+            <td>34,7%</td>
+        </tr>
+    </table>`;
 });
